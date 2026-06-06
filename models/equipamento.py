3d6fea6ea47d8043c3eda989859115ca5fccd7fd
PolianaOnevetch <poliana.teodoro@discente.ufra.edu.br>
2026-06-06 14:17:27 -0300

    Create equipamento.py

---
+++ models/equipamento.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class Equipamento(ABC):
+    id: int
+    nome: str
+    disponivel: bool
+
+    @abstractmethod
+    def calcular_multa(self, dias_atraso):
+        pass
+
+
+@dataclass
+class Notebook(Equipamento):
+
+    def calcular_multa(self, dias_atraso):
+        return float(max(0, dias_atraso * 10))
+
+
+@dataclass
+class Projetor(Equipamento):
+
+    def calcular_multa(self, dias_atraso):
+        return float(max(0, dias_atraso * 15))
+
+
+@dataclass
+class Cabo(Equipamento):
+
+    def calcular_multa(self, dias_atraso):
+        return float(max(0, dias_atraso * 2))
