15b24178c66d636144c640a96df7b82b17846164
Poliana onevetch <Poliana.teodoro@discente.ufra.edu.br>
2026-06-06 17:25:55 -0300

    refactor: simplifica calculo de multa com carencia

--- multa.py
+++ multa.py
@@ -3,9 +3,12 @@ def calcular_multa_com_carencia(
     carencia,
     valor_dia
 ):
-    if dias_atraso <= carencia:
-        return 0.0
+    dias_cobrados = max(
+        0,
+        dias_atraso - carencia
+    )
 
-    excedente = dias_atraso - carencia
-
-    return excedente * valor_dia
+    return round(
+        dias_cobrados * valor_dia,
+        2
+    )
