cfca7f11f278facf1f0b5e461130f33e726d25b0
PolianaOnevetch <poliana.teodoro@discente.ufra.edu.br>
2026-06-06 14:22:05 -0300

    Create notificador.py

---
+++ services/notificador.py
@@ -0,0 +1,34 @@
+class Notificador:
+
+    def notificar_emprestimo(
+        self,
+        email,
+        data_devolucao
+    ):
+
+        print(
+            f"[EMAIL] Empréstimo registrado para "
+            f"{email}. Devolução prevista: "
+            f"{data_devolucao}"
+        )
+
+    def notificar_devolucao(
+        self,
+        email,
+        multa
+    ):
+
+        print(
+            f"[EMAIL] Devolução registrada. "
+            f"Multa: R$ {multa:.2f}"
+        )
+
+    def notificar_atraso(
+        self,
+        email
+    ):
+
+        print(
+            f"[EMAIL] Aviso de atraso enviado "
+            f"para {email}"
+        )
