6c18262b947c9e2350252ab1756e003d42282df3
PolianaOnevetch <poliana.teodoro@discente.ufra.edu.br>
2026-06-06 14:28:13 -0300

    Update interfaces.py

--- repositories/interfaces.py
+++ repositories/interfaces.py
@@ -1,1 +1,38 @@
+from abc import ABC, abstractmethod
 
+
+class InterfaceRepositorioEmprestimo(ABC):
+
+    @abstractmethod
+    def buscar_equipamento(
+        self,
+        equip_id
+    ):
+        pass
+
+    @abstractmethod
+    def salvar_emprestimo(
+        self,
+        emprestimo
+    ):
+        pass
+
+    @abstractmethod
+    def buscar_emprestimos(
+        self
+    ):
+        pass
+
+    @abstractmethod
+    def marcar_indisponivel(
+        self,
+        equip_id
+    ):
+        pass
+
+    @abstractmethod
+    def marcar_disponivel(
+        self,
+        equip_id
+    ):
+        pass
