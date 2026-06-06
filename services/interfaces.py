4cc6ef27e7a249c67789ed64a704243d7d3bcb8d
PolianaOnevetch <poliana.teodoro@discente.ufra.edu.br>
2026-06-06 14:27:39 -0300

    Update interfaces.py

--- services/interfaces.py
+++ services/interfaces.py
@@ -1,1 +1,27 @@
+from abc import ABC, abstractmethod
 
+
+class InterfaceNotificador(ABC):
+
+    @abstractmethod
+    def notificar_emprestimo(
+        self,
+        email,
+        data_devolucao
+    ):
+        pass
+
+    @abstractmethod
+    def notificar_atraso(
+        self,
+        email
+    ):
+        pass
+
+    @abstractmethod
+    def notificar_devolucao(
+        self,
+        email,
+        multa
+    ):
+        pass
