762cce92b7147df3a3f4b3d800b4ac2a9dadd369
PolianaOnevetch <poliana.teodoro@discente.ufra.edu.br>
2026-06-06 14:16:16 -0300

    Create emprestimo.py

---
+++ models/emprestimo.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass
+class Emprestimo:
+    id: int
+    equipamento_id: int
+    nome_usuario: str
+    email: str
+    data_devolucao: date
+    devolvido: bool
+    multa: float = 0.0
